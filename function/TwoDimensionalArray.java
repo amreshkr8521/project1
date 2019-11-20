76af75aba389be1e157ab61e13403db3013515c9
amreshkr8521 <57749252+amreshkr8521@users.noreply.github.com>
2019-11-20 17:38:41 +0530

    updated

--- function/TwoDimensionalArray.java
+++ function/TwoDimensionalArray.java
@@ -3,7 +3,8 @@ package com.bridgelabz.function;
 
 import java.util.Scanner;
 /**
- * 
+ *To take 2D array input and print them to the user 
+ *
  * @author amresh kumar
  * @since 19-11-2019
  * @version 
