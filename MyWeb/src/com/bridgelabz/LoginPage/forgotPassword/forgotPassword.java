cd7d9d40f3f603d597e1ccf261ee0c53eb41c9bb
amreshkr8521 <amresahkr8521@gmail.com>
2020-01-03 15:06:25 +0530

    [amresh kumar].added forgotPassword jsp and java file

---
+++ MyWeb/src/com/bridgelabz/LoginPage/forgotPassword/forgotPassword.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.LoginPage.forgotPassword;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bridgelabz.LoginPage.DAOImplimentation;
+import com.bridgelabz.LoginPage.model.LoginModel;
+
+public class forgotPassword extends HttpServlet{
+
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) {
+		DAOImplimentation daoObject=new DAOImplimentation();
+		String email=request.getParameter("email");
+		String mobile=request.getParameter("mobile");
+		String password=request.getParameter("password");
+		String RePassword=request.getParameter("RePassword");
+		
+		LoginModel loginModel=daoObject.getData(email,mobile);
+		if(loginModel!=null) {
+			loginModel.setPwd(password);
+			int i=daoObject.addUser(loginModel);
+			HttpSession session=request.getSession();
+			session.setAttribute("email","password change");
+			try {
+				response.sendRedirect("LoginPage.jsp");
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
+			
+		}
+		else {
+			request.setAttribute("notchanged","Password not changed");
+			request.getRequestDispatcher("forgotPassword.jsp");
+		}
+	}
+}
